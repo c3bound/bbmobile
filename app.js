f144cd1d4060fc75866afd0ba8cb3e146394d6a8
Dustin LeBleu <dustin@mathenomics.com>
2016-11-12 11:20:45 -0600

    s

--- app.js
+++ app.js
@@ -72,17 +72,7 @@ http.createServer(function (req, res) {
 
 function createWebpage (req, res) {
   // Let's find all the documents
-  PUser.find({}).exec(function(err, result) { 
-    if (!err) { 
-      res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
-      
-      query.exec(function(err, result) {
-	
-      });
-    } else {
-      res.end('Error in first query. ' + err)
-    };
-  });
+  
 }
 
 // Tell the console we're getting ready.
