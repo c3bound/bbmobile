06d0d4cd6fc1c807b039512fae5fa8b22a6657c9
Dustin LeBleu <dustin@mathenomics.com>
2016-11-12 11:25:51 -0600

    fd

--- app.js
+++ app.js
@@ -41,11 +41,11 @@ var mongoose = require ("mongoose"); // The reason for this demo.
 // localhost if we don't find one.  
 var uristring = 
   process.env.MONGODB_URI || 
-  'mongodb://216.150.149.12:27017/BlackBoxBeta';
+  'mongodb://localhost/HelloMongoose';
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
-var theport = process.env.PORT || 27017;
+var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
@@ -57,7 +57,52 @@ mongoose.connect(uristring, function (err, res) {
   }
 });
 
+// This is the schema.  Note the types, validation and trim
+// statements.  They enforce useful constraints on the data.
+var userSchema = new mongoose.Schema({
+  name: {
+    first: String,
+    last: { type: String, trim: true }
+  },
+  age: { type: Number, min: 0}
+});
 
+// Compiles the schema into a model, opening (or creating, if
+// nonexistent) the 'PowerUsers' collection in the MongoDB database
+var PUser = mongoose.model('PowerUsers', userSchema);
+
+// Clear out old data
+PUser.remove({}, function(err) {
+  if (err) {
+    console.log ('error deleting old data.');
+  }
+});
+
+// Creating one user.
+var johndoe = new PUser ({
+  name: { first: 'John', last: 'Doe' },
+  age: 25
+});
+
+// Saving it to the database.  
+johndoe.save(function (err) {if (err) console.log ('Error on save!')});
+
+// Creating more users manually
+var janedoe = new PUser ({
+  name: { first: 'Jane', last: 'Doe' },
+  age: 65
+});
+janedoe.save(function (err) {if (err) console.log ('Error on save!')});
+
+// Creating more users manually
+var alicesmith = new PUser ({
+  name: { first: 'Alice', last: 'Smith' },
+  age: 45
+});
+alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
+
+
+// In case the browser connects before the database is connected, the
 // user will see this message.
 var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
 
@@ -70,7 +115,26 @@ http.createServer(function (req, res) {
   createWebpage(req, res);
 }).listen(theport);
 
-
+function createWebpage (req, res) {
+  // Let's find all the documents
+  PUser.find({}).exec(function(err, result) { 
+    if (!err) { 
+      res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
+      // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
+      var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
+      query.where('age').gt(64);
+      query.exec(function(err, result) {
+	if (!err) {
+	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
+	} else {
+	  res.end('Error in second query. ' + err)
+	}
+      });
+    } else {
+      res.end('Error in first query. ' + err)
+    };
+  });
+}
 
 // Tell the console we're getting ready.
 // The listener in http.createServer should still be active after these messages are emitted.
