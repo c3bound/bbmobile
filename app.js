fa659019a7809209cba11896bef6f620a920de2a
Dustin LeBleu <dustin@mathenomics.com>
2016-11-13 16:37:51 -0600

    test

--- app.js
+++ app.js
@@ -34,14 +34,28 @@
 
 //
 // Preamble
-var mongoose = require('mongoose');
 
-var db = mongoose.connection;
 
-db.on('error', console.error);
-db.once('open', function() {
-  // Create your schemas and models here.
+var http = require ('http');	     // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.  
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://215.150.149.11:27017/BlackBoxBeta';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
 });
-
-mongoose.connect('mongodb://215.150.149.11:27017/BlackBoxBeta');
 
