0518c1fe0200df2de16dce56249c315d699c0068
Dustin LeBleu <dustin@mathenomics.com>
2016-11-13 20:14:18 -0600

    test

--- app.js
+++ app.js
@@ -36,26 +36,112 @@
 // Preamble
 
 
+var http = require ('http');	     // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.  
+var uristring = 'mongodb://216.150.149.11:27017/BlackBoxBeta';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
+// This is the schema.  Note the types, validation and trim
+// statements.  They enforce useful constraints on the data.
+var userSchema = new mongoose.Schema({
+	
+	_id : String,
+    Symbol : String,
+    CreatedDttm : String,
+    MessageType : String,
+    DateTime : String,
+    PriceAtAlert : String,
+    Message : String,
+    Exchange : String
+  
+});
+
+// Compiles the schema into a model, opening (or creating, if
+// nonexistent) the 'PowerUsers' collection in the MongoDB database
+var Stocks = mongoose.model('AlertStreamNasdaq', userSchema);
 
 
 
-var mongoose = require('mongoose')
-  , MONGO_DB = 'mongodb://216.150.149.11:27017/BlackBoxBeta';
+// In case the browser connects before the database is connected, the
+// user will see this message.
+var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
 
-mongoose.connect(MONGO_DB);
+// Create a rudimentary http server.  (Note, a real web application
+// would use a complete web framework and router like express.js). 
+// This is effectively the main interaction loop for the application. 
+// As new http requests arrive, the callback function gets invoked.
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  createWebpage(req, res);
+}).listen(theport);
 
-mongoose.connection.on('open', function(){
-  mongoose.connection.db.collectionNames(function(error, collections) {
-    if (error) {
-      throw new Error(error);
-    } else {
-      collections.map(function(collection) {
-        console.log('found collection %s', collection.name);
+function createWebpage (req, res) {
+  // Let's find all the documents
+  Stocks.find(function (err, result) { 
+    if (!err) { 
+      res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
+      // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
+      var query = Stocks.find({}); // (ok in this example, it's all entries)
+      query.where('MessageType').gt(5);
+      query.exec(function(err, result) {
+	if (!err) {
+	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
+	} else {
+	  res.end('Error in second query. ' + err)
+	}
       });
-    }
+    } else {
+      res.end('Error in first query. ' + err)
+    };
   });
-});
 
-mongoose.connection.on('error', function(error){
-  throw new Error(error);
-});
+
+
+}
+
+// Tell the console we're getting ready.
+// The listener in http.createServer should still be active after these messages are emitted.
+console.log('http server will be listening on port %d', theport);
+console.log('CTRL+C to exit');
+
+//
+// House keeping.
+
+//
+// The rudimentary HTML content in three pieces.
+var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
+<head> \
+<style> body {color: #394a5f; font-family: sans-serif} </style> \
+</head> \
+<body> \
+<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
+See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
+<br\> \
+<br\> \
+<br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
+var html2 = '</code></pre> <br\> <i>';
+var html3 = ' documents. </i> <br\> <br\>';
+var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
+var html5 = '</code></pre> <br\> <i>';
+var html6 = ' documents. </i> <br\> <br\> \
+<br\> <br\> <center><i> Demo code available at  </i></center>';
+
+
+
