d2f8b9b6964f2e513a0d9f038e199975f39c7fad
Dustin LeBleu <dustin@mathenomics.com>
2016-11-13 18:55:28 -0600

    tes

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ function createWebpage (req, res) {
     if (!err) { 
       res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
       // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
-      var query = Stocks.find({}); // (ok in this example, it's all entries)
+      var query = Stocks.find({'Symbol': '*'}); // (ok in this example, it's all entries)
       query.where('MessageType').gt(5);
       query.exec(function(err, result) {
 	if (!err) {
